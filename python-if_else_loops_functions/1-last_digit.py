f59d6e770f8c34ddfd71bf54bd1922012c2c6a5c
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-01-14 17:22:05 +0300

    % used and figure out the signe also

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+no = number % 10
+if number < 0:
+    no = no - 10
+if no > 5:
+    print(f"Last digit of {number} is {no} and is greater than 5")
+elif no < 6 and no != 0:
+    print(f"Last digit of {number} is {no} and is less than 6 and not 0")
+else:
+    print(f"Last digit of {number} is {no} and is 0")
