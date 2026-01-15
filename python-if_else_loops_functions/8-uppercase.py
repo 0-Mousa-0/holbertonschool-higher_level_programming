c0ef0f816720cd0e74bcb294fdd0090f56b950a4
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-01-15 11:53:24 +0300

    no more then two print in the code

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord(c) in range(97, 123):
-            print("{}".format(chr(ord(c) - 32)), end="")
-        else:
-            print("{}".format(chr(ord(c))), end="")
+        print("{}".format(chr(ord(c) - 32) if ord(c) in range(97, 123) else c), end="")
+    print()
 
-    print("")
+      #  if ord(c) in range(97, 123):
+     #       print("{}".format(chr(ord(c) - 32)), end="")
+    #    else:
+   #         print("{}".format(chr(ord(c))), end="")
+
+    #print("")
