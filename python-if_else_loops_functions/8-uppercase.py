84ad5ad9cb3a40b78c70e7c9861721cefb659d22
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-01-15 11:36:14 +0300

    upper case statments using ord()

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        if ord(c) in range(97, 123):
+            print("{}".format(chr(ord(c) - 32)), end="")
+        else:
+            print("{}".format(chr(ord(c))), end="")
+
+    print("")
