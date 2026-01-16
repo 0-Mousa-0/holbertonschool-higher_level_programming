7df2c10bfcc404ab0a283f599c793d86d9f83195
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-01-16 13:56:14 +0300

    import the function add only

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-import add_0
-
-
-def main():
-
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+from add_0 import add
 
 
 if __name__ == "__main__":
-    main()
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
