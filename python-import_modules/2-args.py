b0205d9fb171e2810423224912b0b11eb1cca9af
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-01-16 16:04:12 +0300

    import sys class to see from the terminal and insure that you exit from it after take the whole vector arg

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+len = len(sys.argv) - 1
+i = 1
+if len == 0:
+    print(f"{len} arguments.")
+    sys.exit(0)
+else:
+    print(f"{len} arguments:")
+    while i <= len:
+        print(f"{i}: " + sys.argv[i])
+
+        i = i + 1
+    sys.exit(0)
