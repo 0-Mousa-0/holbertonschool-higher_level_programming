fc69cc6ebd9be3cad9db7fe05e1784822729ed10
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-01-16 16:08:43 +0300

    add __main__ to run only when excuted

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 import sys
+if __name__ == "__main__":
+    len = len(sys.argv) - 1
+    i = 1
+    if len == 0:
+        print(f"{len} arguments.")
+        sys.exit(0)
+    else:
+        print(f"{len} arguments:")
+        while i <= len:
+            print(f"{i}: " + sys.argv[i])
 
-len = len(sys.argv) - 1
-i = 1
-if len == 0:
-    print(f"{len} arguments.")
-    sys.exit(0)
-else:
-    print(f"{len} arguments:")
-    while i <= len:
-        print(f"{i}: " + sys.argv[i])
-
-        i = i + 1
-    sys.exit(0)
+            i = i + 1
+        sys.exit(0)
