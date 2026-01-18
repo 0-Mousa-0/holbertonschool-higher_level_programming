11cdbad26888461d2bc63c9ce415a8dea37f4de6
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-01-18 11:54:58 +0300

    nested loop, enumerate() function, row in matrix to get the row el

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+
+    for row in matrix:
+        for i, num in enumerate(row):
+            if not num:
+                print("")
+            print("{} ".format(num), end="")
+            i += 1
+        print()
