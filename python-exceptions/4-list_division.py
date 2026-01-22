29244fc1352a6e2f9fa92a71846629e4b1f81d72
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-01-22 14:12:29 +0300

    fixing the printed comands for excption

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -6,7 +6,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
-            print("division by {}: ".format(my_list_2[i]))
+            print("division by {} ".format(my_list_2[i]))
             result = 0
         except TypeError:
             print("wrong type")
