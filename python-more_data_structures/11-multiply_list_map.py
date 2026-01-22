7bb46b6d15cd2f55c2b0cbc97961c9aacc965d53
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-01-22 10:04:14 +0300

    no loop now map() used

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    listm = []
-    for i in my_list:
-        listm.append(i * number)
-    return listm
+    return list(map(lambda i: i * number, my_list))
