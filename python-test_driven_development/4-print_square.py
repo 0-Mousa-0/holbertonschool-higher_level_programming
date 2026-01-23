822d906305fda719c1228bd94d6e7901ab1ef584
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-01-23 20:32:13 +0300

    arange the conditions

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -8,13 +8,13 @@ def print_square(size):
     """
     :param size: size of the square rows & columns
     """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    elif not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    elif size == 0:
+    if size == 0:
         pass
-    elif size > 0:
+    if size > 0:
         for i in range(size):
             print("#" * size)
