aa4c4e09dface8c82c2125dd4b2950add7ae0a79
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-01-23 17:13:50 +0300

    conditions cleaned now typed in correctly way (isinstance)

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -3,6 +3,8 @@
 module to divide a matrix elements
 by a number
 """
+
+
 def matrix_divided(matrix, div):
     """
 
@@ -12,24 +14,28 @@ def matrix_divided(matrix, div):
     """
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    if div != int(div) and div != float(div):
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-
-    r = matrix[0]
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/"
+                        "floats")
+    if not matrix:
+        return []
+    r = len(matrix[0])
     rm = []
 
     for row in matrix:
-        if row != list(row):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        if len(r) != len(row):
+
+        if r != len(row):
             raise TypeError("Each row of the matrix must have the same size")
         result = []
         for i in row:
             if not isinstance(i, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of integers/"
+                    "floats"
+                )
             result.append(round(i / div, 2))
         rm.append(result)
-        r = row
+
     return rm
-
-
