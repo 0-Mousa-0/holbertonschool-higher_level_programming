d709a5c7010967bb2feac32548e7aa7bc86a24d0
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-01-23 21:13:22 +0300

    no idea what is the problem

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
 """
-this module is to print text in correctly way
+module print the text in a role
 """
 
 
 def text_indentation(text):
     """
-    :param text: to be printed in correctly way
+
+    :param text: to be printed
     :return: nothing
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    line = ""
-    if isinstance(text, str):
-        for char in text:
-            if char == "." or char == "?" or char == ":":
-                line += char
-                print(line, end="\n\n")
-                line = ""
-        
-            elif not line and char == " ":
-                continue
-            else:
-                line += char
-        if line:
-            print(line, end="")
+
+    i = 0
+    while i < len(text):
+        if text[i] == " " and (i == 0 or text[i - 1] in ".?:"):
+            i += 1
+            continue
+
+        print(text[i], end="")
+
+        if text[i] in ".?:":
+            print("\n")
+            i += 1
+            while i < len(text) and text[i] == " ":
+                i += 1
+            continue
+
+        i += 1
