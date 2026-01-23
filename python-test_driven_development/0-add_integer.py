5ff63a283c0e7364599f2422081d35620661a3bd
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-01-23 16:03:48 +0300

    all checks conditions to a,b added >> not infinity or not equal itself

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -11,10 +11,10 @@ def add_integer(a, b=98):
     a and b must be integers or floats
     Returns an integer
     """
-    if not isinstance(a, (int, float)):
+    if not isinstance(a, (int, float)) or a == float('inf' or '-inf') or a != a:
         raise TypeError("a must be an integer")
 
-    if not isinstance(b, (int, float)):
+    if not isinstance(b, (int, float)) or b == float('inf' or '-inf') or b != b:
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
