17a9323557a9eea76815eb784249f1c3e162360f
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-01-30 21:11:44 +0300

    fix the condition type of value

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -20,7 +20,7 @@ class BaseGeometry:
         :param value: number
         :return: its can be an inputs or not
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
