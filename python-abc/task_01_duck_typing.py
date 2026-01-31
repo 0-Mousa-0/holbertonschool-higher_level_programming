aa8b24084d4cb0d8bfd8503c81ff08b22c1d1a57
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-01-31 23:42:13 +0300

    code style

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -21,12 +21,24 @@ class Circle(Shape):
     """inherit from Shape"""
 
     def __init__(self, radius=0):
+        """
+
+        :param radius:value
+        """
         self.radius = radius
 
     def area(self):
+        """
+
+        :return: result
+        """
         return math.pi * (self.radius**2)
 
     def perimeter(self):
+        """
+
+        :return: result
+        """
         return self.radius * 2 * math.pi
 
 
@@ -34,16 +46,34 @@ class Rectangle(Shape):
     """inherit from Shape"""
 
     def __init__(self, width=0, height=0):
+        """
+
+        :param width: value
+        :param height: value
+        """
         self.width = width
         self.height = height
 
     def area(self):
+        """
+
+        :return: result
+        """
         return self.width * self.height
 
     def perimeter(self):
+        """
+
+        :return: result
+        """
         return 2 * (self.width + self.height)
 
 
 def shape_info(Shape):
+    """
+
+    :param Shape: abc class
+    :return: print of infos
+    """
     print(f"Area: {Shape.area()}")
     print(f"Perimeter: {Shape.perimeter()}")
