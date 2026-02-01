c68e7a0c017022af13f21bf5b7311628d107631c
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-02-01 14:12:32 +0300

    discrebtion of red file method added

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,5 +5,6 @@ read - write - excute .. files
 
 
 def read_file(filename=""):
+    """Read a UTF-8 text file and print its content to stdout"""
     with open(filename, 'r' ,encoding='utf-8') as f:
         print(f.read(), end="")
