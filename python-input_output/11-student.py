03df68df1d88cec99773beb327fd5053594de5ff
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-02-04 12:13:51 +0300

    convert dictionary to object by .update(json)

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""save the object elements"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        """ "assign the attributes values"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """python object in dictionary format"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            newDict = {}
+            for attr in attrs:
+                if isinstance(attr, str) and attr in self.__dict__:
+                    newDict[attr] = self.__dict__[attr]
+
+            return newDict
+
+    def reload_from_json(self, json):
+        """reload the attributes from json"""
+        self.__dict__.update(json)
