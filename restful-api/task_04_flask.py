cae0f94ef43879d057e5491a00143af2c39e650f
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-02-13 14:14:51 +0300

    handel deffrent routes with url server, post via url, search on name

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""first use of framework flask"""
+
+from flask import Flask
+from flask import jsonify, request
+
+app = Flask(__name__)
+users = {}
+@app.route('/')
+def home():
+    return 'Welcome to the Flask API!'
+@app.route('/status')
+def status():
+    return 'ok'
+@app.route('/data')
+def data():
+    return jsonify(list(users.keys()))
+@app.route('/users/<username>')
+def users(username):
+    user = users.get(username)
+    if user is None:
+        return jsonify({"error": "User not found"}), 404
+    return jsonify(user)
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    if not request.is_json:
+        return jsonify({"error": "Invalid JSON"}), 400
+
+    data = request.get_json()
+
+    username = data.get("username")
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+
+    if username in users:
+        return jsonify({"error": "Username already exists"}), 409
+
+    users[username] = data
+
+    return jsonify({
+        "message": "User added",
+        "user": data
+    }), 201
+
+
+
+
+if __name__ == '__main__':
+    app.run()
