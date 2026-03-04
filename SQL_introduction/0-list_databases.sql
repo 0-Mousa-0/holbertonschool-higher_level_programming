75c13d291a35a3a5f579edc6c4cfc946c0ae65c6
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-03-04 13:55:43 +0300

    conflict solved

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -2,7 +2,4 @@
  --list all databases in mysql server
  SHOW DATABASES;
       
-=======
--- Task 0: list all databases of the MySQL server.
-SHOW DATABASES; -- Display every database available on this MySQL server.
->>>>>>> a2fdb4a9e5f570e213b2dbac71c444e23b324593
+
